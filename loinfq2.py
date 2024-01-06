13c4849de8134f61c4c0845f2e31802838c0e280
526awd <62787078+526awd@users.noreply.github.com>
2024-01-06 21:36:00 +0800

    4

---
+++ loinfq2.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlencode
+import requests
+import time
+import re
+import json
+def qr():
+    h={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
+    r=requests.get('https://passport.bilibili.com/x/passport-login/web/qrcode/generate',headers=h)
+    print(r.text)
+    f=r.text
+    f=re.search(r'({.*})',f).group(0)
+    j=json.loads(f)
+    curli = str(j['data']["url"])
+    ll=str(j['data']['qrcode_key'])
+    vep={'qrcode_key':ll}
+    l ={'url':curli}
+    curll= urlencode(l)
+    jpgg=requests.get('https://api.pwmqr.com/qrcode/create/',params=curll)
+    f=open('ddd.jpg','wb+')
+    f.write(jpgg.content)
+    f.close()
+    print(j['data']["url"])
+    print
+
+    while True:
+        sq=requests.get('https://passport.bilibili.com/x/passport-login/web/qrcode/poll',params=vep,headers=h)
+        sqq=re.search(r'({.*})',sq.text).group(0)
+        sqqj=json.loads(sqq)
+        if sqqj['data']['code']==0:
+            print('完成')
+            return sq.cookies
+            break
+        else :
+            print(sqqj['data']['code'])
+            time.sleep(2.5)
+
---
+++ smsling.py
@@ -0,0 +1,10 @@
+#import requests
+#import os
+#import json
+#phone=input('请输入你的手机号\n')
+#h={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
+####f=re.search(r'({.*})',c.text).group(0)
+#j=json.loads(f)
+#j
+#os.system('cmd.exe /c start https://kuresaru.github.io/geetest-validator/')
+#q=input('')
